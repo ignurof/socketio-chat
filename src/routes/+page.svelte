6333e5127f99ae8446a64b201019dc3611e153f0
ignurof <ignurof@ignurof.com>
2023-06-26 01:02:52 +0200

    svelte code example hello world

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Hello, World!</h1>
-<p>made by ignurof, check out my <a href="https://github.com/ignurof">github</a> to find the source code.</p>
+<p>made by ignurof, check out my <a href="https://github.com/ignurof" target="_BLANK">github</a> to find the source code.</p>
+
+<script>
+	console.log("Hello, World!");
+</script>
+
+<style>
+	p {
+		background-color: red;
+	}
+</style>
